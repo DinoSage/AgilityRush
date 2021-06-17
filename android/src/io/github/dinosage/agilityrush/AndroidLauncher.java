223ded7f900119b7f9879868bc96294a3c4d85d6
DinoMaster9900 <54876937+DinoMaster9900@users.noreply.github.com>
2021-06-17 16:53:37 -0700

    Import + Project Structure Changes

---
+++ android/src/io/github/dinosage/agilityrush/AndroidLauncher.java
@@ -0,0 +1,15 @@
+package io.github.dinosage.agilityrush;
+
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.dinosage.agilityrush.app.GameStart;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameStart(), config);
+	}
+}
